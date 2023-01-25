2af2919be0bc75e0082615ed69616556bb23c857
Hany Medhat <hany.medhat24@gmail.com>
2023-01-25 13:16:45 +0200

    create cubit

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/custom_button.dart';
 import 'package:notes_app/views/widgets/custom_text_field.dart';
 
 class AddNoteBottomsheet extends StatelessWidget {
@@ -6,22 +7,64 @@ class AddNoteBottomsheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: AddNoteForm(),
+    );
+  }
+}
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> fromKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  String? title, subTitle;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: fromKey,
+      autovalidateMode: autovalidateMode,
       child: Column(
-        children:const [
-          SizedBox(
+        children: [
+          const SizedBox(
             height: 32,
           ),
           CustomTextField(
+            onSaved: (value) {
+              title = value;
+            },
             hint: 'Title',
           ),
-          SizedBox(height: 16,),
+          const SizedBox(
+            height: 16,
+          ),
           CustomTextField(
+            onSaved: (value) {
+              subTitle = value;
+            },
             hint: 'Content',
             maxLines: 5,
           ),
-
+          const SizedBox(
+            height: 32,
+          ),
+          CustomButton(
+            onTap: () {
+              if (fromKey.currentState!.validate()) {
+                fromKey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+              }
+            },
+          ),
         ],
       ),
     );
