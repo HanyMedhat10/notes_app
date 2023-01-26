f94f4015308e5525a082983126819ce425ed7540
Hany Medhat <hany.medhat24@gmail.com>
2023-01-26 15:02:27 +0200

    create fetch note mothed

--- lib/cubits/add_note_cubit/notes_cubit/notes_cubit.dart
+++ lib/cubits/add_note_cubit/notes_cubit/notes_cubit.dart
@@ -1,5 +1,7 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:meta/meta.dart';
+import 'package:notes_app/constsants.dart';
 
 import '../../../models/note_models.dart';
 
@@ -7,4 +9,13 @@ part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+  fetchAllNotes()  {
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+       List<NoteModel> notes =notesBox.values.toList();
+      emit(NotesSuccess(notes));
+    } catch (e) {
+      emit(NotesFailure(e.toString()));
+    }
+  }
 }
--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,25 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/add_note_cubit/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/views/widgets/custom_app_bar.dart';
 import 'package:notes_app/views/widgets/custom_note_item.dart';
 import 'package:notes_app/views/widgets/notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
-const NotesViewBody({ Key? key }) : super(key: key);
+  const NotesViewBody({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: Column(
-        children:const [
-          SizedBox(
-            height: 50,
-          ),
-          CustomAppBar(title: 'Notes',icon: Icons.search,),
-          Expanded(child: NotesListView()),
-        ],
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          children: const [
+            SizedBox(
+              height: 50,
+            ),
+            CustomAppBar(
+              title: 'Notes',
+              icon: Icons.search,
+            ),
+            Expanded(child: NotesListView()),
+          ],
+        ),
       ),
     );
   }
 }
-
